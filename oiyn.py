518130222840355f61166c12972d221bb2f8a416
NurDAULET <nurik.02311@gmail.com>
2021-12-21 17:21:55 +0600

    dop qosyldy

--- oiyn.py
+++ oiyn.py
@@ -23,7 +23,10 @@ YELLOWGHOSTCUT_STARTING_VELOCITY = 9
 player_score = PLAYER_STARTING_SCORE
 player_live = PLAYER_STARTING_LIVE
 cut_velocity = 5
+ghostpck_velocity = GHOSTPCK_STARTING_VELOCITY
 
+yellowghostcut_velocity = YELLOWGHOSTCUT_STARTING_VELOCITY
+acceleration = 0.5
 
 # main surface
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -46,6 +49,12 @@ cut_image = pygame.image.load('glv.PNG').convert_alpha()
 cut_image = pygame.transform.scale(cut_image, (50, 80))  
 cut_rect = cut_image.get_rect()
 cut_rect.center = (80, WINDOW_HEIGHT//2)
+
+ghostpck_image = pygame.image.load('dop.PNG').convert_alpha()
+ghostpck_image = pygame.transform.scale(ghostpck_image, (40, 60)) 
+ghostpck_image_rect = ghostpck_image.get_rect()
+ghostpck_image_rect.center = ((WINDOW_WIDTH+BUFFER_DISTANCE), random.randint(HEADER_HEIGHT+25, WINDOW_HEIGHT-25))
+
 
 
 
@@ -100,9 +109,28 @@ while running:
     elif keys[pygame.K_DOWN] and cut_rect.bottom < WINDOW_HEIGHT:
         cut_rect.y += ghostpck_velocity
 
+    ghostpck_image_rect.centerx -= ghostpck_velocity
 
 
-  
+
+    if ghostpck_image_rect.centerx < 0:
+        ghostpck_image_rect.center = ((WINDOW_WIDTH+BUFFER_DISTANCE), random.randint(HEADER_HEIGHT+25, WINDOW_HEIGHT-25))
+        player_live -= 1
+        sound2.play()
+
+
+
+
+
+    if cut_rect.colliderect(ghostpck_image_rect):
+        player_score += 1
+        sound1.play()
+        ghostpck_image_rect.center = ((WINDOW_WIDTH+BUFFER_DISTANCE), random.randint(HEADER_HEIGHT+25, WINDOW_HEIGHT-25))
+        ghostpck_velocity += acceleration
+
+    
+
+
         
     if player_live == 0 or player_score >15:
         fl_pause = True
@@ -133,7 +161,7 @@ while running:
     display_surface.blit(live_background, live_background_rect)
     pygame.draw.line(display_surface, WHITE, (0, HEADER_HEIGHT), (WINDOW_WIDTH, HEADER_HEIGHT), 5)
     display_surface.blit(cut_image, cut_rect)
-
+    display_surface.blit(ghostpck_image, ghostpck_image_rect)
     
     score_text = main_font.render("OCHKO: " + str(player_score), True, RED)
     live_text = main_font.render("ZHANY: " + str(player_live), True, RED)
