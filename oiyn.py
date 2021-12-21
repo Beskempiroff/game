f59b09bd1323ef6eea56e16f95c1d36178614486
NurDAULET <nurik.02311@gmail.com>
2021-12-21 17:17:34 +0600

    O4ko, zhann qosyldy

--- oiyn.py
+++ oiyn.py
@@ -16,11 +16,14 @@ DARKGREEN = (10, 50, 10)
 BUFFER_DISTANCE = 100
 PLAYER_STARTING_SCORE = 0
 PLAYER_STARTING_LIVE = 5
+GHOSTPCK_STARTING_VELOCITY = 5
 
+YELLOWGHOSTCUT_STARTING_VELOCITY = 9
 
 player_score = PLAYER_STARTING_SCORE
 player_live = PLAYER_STARTING_LIVE
 cut_velocity = 5
+
 
 # main surface
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -30,6 +33,14 @@ pygame.display.set_caption("CuT")
 background = pygame.image.load('fon.jpeg')
 
 
+
+score_backround = pygame.image.load('score_background.png').convert_alpha()
+score_background_rect = score_backround.get_rect()
+score_background_rect.topleft = (10,10)
+
+live_background = pygame.image.load('score_background.png').convert_alpha()
+live_background_rect = live_background.get_rect()
+live_background_rect.topleft = (WINDOW_WIDTH-(live_background_rect.width+10), 10)
 
 cut_image = pygame.image.load('glv.PNG').convert_alpha()
 cut_image = pygame.transform.scale(cut_image, (50, 80))  
@@ -45,9 +56,25 @@ cut_rect.center = (80, WINDOW_HEIGHT//2)
 # fonts and texts
 main_font = pygame.font.Font('AttackGraffiti.ttf', 32)
 
+score_text = main_font.render("BALL: " + str(player_score), True, RED)
+score_text_rect = score_text.get_rect()
+score_text_rect.topleft = (20, 20)
 
+game_name = main_font.render("CuT", True, RED, WHITE)
+game_name_rect = game_name.get_rect()
+game_name_rect.center = (WINDOW_WIDTH//2, HEADER_HEIGHT//2)
 
+live_text = main_font.render("OIRI: " + str(player_live), True, RED)
+live_text_rect = live_text.get_rect()
+live_text_rect.topleft = (WINDOW_WIDTH-(live_background_rect.width), 20)
 
+game_over_text = main_font.render("OIYN AYAQTALDY", True, GREEN, DARKGREEN)
+game_over_rect = game_over_text.get_rect()
+game_over_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
+
+game_continue_text = main_font.render("QAITADAN", True, GREEN)
+game_continue_rect = game_continue_text.get_rect()
+game_continue_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2+100)
 
 # sounds and musics
 pygame.mixer.music.load('music.wav')
@@ -68,13 +95,20 @@ while running:
             running = False
 
     keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP] and cut_rect.top > HEADER_HEIGHT:
+        cut_rect.y -= ghostpck_velocity
+    elif keys[pygame.K_DOWN] and cut_rect.bottom < WINDOW_HEIGHT:
+        cut_rect.y += ghostpck_velocity
 
 
+
+  
         
     if player_live == 0 or player_score >15:
         fl_pause = True
         pygame.mixer.music.stop()
         live_text = main_font.render("ZHANY: " + str(player_live), True, RED)
+        display_surface.blit(live_text, live_text_rect)
         pygame.display.update()
         while fl_pause:
             for event in pygame.event.get():
@@ -84,22 +118,28 @@ while running:
                 if event.type == pygame.KEYDOWN:
                     player_score = PLAYER_STARTING_SCORE
                     player_live = PLAYER_STARTING_LIVE
+                    ghostpck_velocity = GHOSTPCK_STARTING_VELOCITY
+                    yellowghostcut_velocity = GHOSTPCK_STARTING_VELOCITY
                     pygame.mixer.music.play(-1, 0.0) 
                     fl_pause = False
 
-      
-     
+            display_surface.blit(game_over_text, game_over_rect)
+            display_surface.blit(game_continue_text, game_continue_rect)
             pygame.display.update()
 
 
     display_surface.blit(background, (0,0))
+    display_surface.blit(score_backround, score_background_rect)
+    display_surface.blit(live_background, live_background_rect)
     pygame.draw.line(display_surface, WHITE, (0, HEADER_HEIGHT), (WINDOW_WIDTH, HEADER_HEIGHT), 5)
     display_surface.blit(cut_image, cut_rect)
 
     
-
-
-
+    score_text = main_font.render("OCHKO: " + str(player_score), True, RED)
+    live_text = main_font.render("ZHANY: " + str(player_live), True, RED)
+    display_surface.blit(score_text, score_text_rect)
+    display_surface.blit(game_name, game_name_rect)
+    display_surface.blit(live_text, live_text_rect)
     
 
 
